bc99fea411cd9598f2be8dd745db9e469b9b3cf3
Leonid Medovyy <leonid@logik.works>
2020-04-28 12:54:38 -0700

    api mocked

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resource :builds
+      resource :builds, only: [:create] do
+        post :status, on: :member
+        post :update_url, on: :member
+        post :disable, on: :member
+      end
     end
   end
 
